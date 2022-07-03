95a2f4b5a916a6801c0fc5b554295548a957ce5a
musharaf <freelancer.musharaf@gmail.com>
2022-07-03 21:55:52 +0600

    character couter create

---
+++ main.js
@@ -0,0 +1,13 @@
+let txt = document.querySelector("#txt");
+let result = document.querySelector(".result");
+
+txt.addEventListener("input", function(){
+
+    let character = txt.value.length;
+    if(character > 10){
+     
+        txt.style.color = "red";
+
+    }
+    result.innerText = character;
+})
